4bb23fbf856bd79199315b9342edc55464363ab2
ayo <0xosepatrick@gmail.com>
2025-04-14 12:29:09 +0100

    add express types

--- @types/express/express.d.ts
+++ @types/express/express.d.ts
@@ -1,9 +1,11 @@
-import * as express from "express";
+import { Request } from "express";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: any;
+      user?: {
+        uniqueId: string;
+      };
     }
   }
 }
