0a501e08e64a559fbe5c5d97d8c09b8a9b9b0204
ayo <0xosepatrick@gmail.com>
2025-04-15 18:00:49 +0100

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import userRoutes from "./user.routes";
 import loginRoutes from "./user.routes";
+import forgotPasswordRoute from "./user.routes";
+import changePasswordRoutes from "./user.routes";
 
-export { userRoutes, loginRoutes };
+export { userRoutes, loginRoutes, changePasswordRoutes, forgotPasswordRoute };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/user.controller";
-import { validateUser, validateLogin } from "../middleware/validate-user";
+import { forgotPassword } from "../controllers/password.controller";
+import {
+  validateUser,
+  validateLogin,
+  validateChangePassword,
+} from "../middleware/validate-user";
+import { validateForgotPassword } from "../middleware/validate-password";
+// import { authenticateJWT } from "../middleware/auth";
+// import { changePassword } from "../controllers/users-flows.controller";
 
 const router = Router();
 
 router.post("/create-user", validateUser, createUser);
 router.post("/login", validateLogin, loginUser);
+
+router.post("/forgot-password", validateForgotPassword, forgotPassword);
+
+// Defines the change password route
+// router.post(
+//   "/change-password",
+//   authenticateJWT,
+//   validateChangePassword,
+//   changePassword
+// );
 
 export default router;
